02fa5786b37ea5a57fdc587ab762d3db677ad583
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2024-03-17 03:55:04 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    setTodo([...todo, text]);
+    setText("");
+  };
+  const handleDelete = (idx) => {
+    const newTodo = [...todo];
+    newTodo.splice(idx, 1);
+    setTodo(newTodo);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className="todo">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={handleClick}>Add Todo</button>
+      </div>
+      <div className="todo-item">
+        <ul>
+          {todo.map((item, idx) => (
+            <li key={idx}>
+              {item}{" "}
+              <button className="btn" onClick={handleDelete}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
